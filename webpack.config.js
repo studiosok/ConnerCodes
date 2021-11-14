611a5a85af3c12fd8a74a6031f1c4e91df06fcdb
Kristen Studioso <45612650+studiosok@users.noreply.github.com>
2021-11-14 18:06:33 -0500

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlDev = "https://condescending-goldberg-8f6337.netlify.app/";
+const urlProd = "https://condescending-goldberg-8f6337.netlify.app/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
